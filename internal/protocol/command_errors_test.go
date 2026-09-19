f276b341f417a3702d136b4514fe9533616f4c5a
rider3458 <duducmanh2002@gmail.com>
2026-09-20 02:06:36 +0700

    Add command error encoder tests

--- internal/protocol/command_errors_test.go
+++ internal/protocol/command_errors_test.go
@@ -3,5 +3,38 @@ package protocol
 import "testing"
 
 func TestCommandErrorEncoders(t *testing.T) {
-	t.Skip("TODO: add command error encoder tests")
+	tests := []struct {
+		name string
+		got  []byte
+		want string
+	}{
+		{
+			name: "unknown command",
+			got:  EncodeUnknownCommand("FOO"),
+			want: "-ERR unknown command 'FOO'\r\n",
+		},
+		{
+			name: "wrong arity",
+			got:  EncodeWrongArity("set"),
+			want: "-ERR wrong number of arguments for 'set' command\r\n",
+		},
+		{
+			name: "not integer",
+			got:  EncodeNotInteger(),
+			want: "-ERR value is not an integer or out of range\r\n",
+		},
+		{
+			name: "wrong type",
+			got:  EncodeWrongType(),
+			want: "-WRONGTYPE Operation against a key holding the wrong kind of value\r\n",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if string(tt.got) != tt.want {
+				t.Fatalf("encoder = %q, want %q", string(tt.got), tt.want)
+			}
+		})
+	}
 }
